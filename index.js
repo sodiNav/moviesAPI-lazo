56dbd077aadec5f6ed7decf56ad40acc86a382cb
jamesivnlazo <jamesivnlazo@gmail.com>
2026-06-17 15:52:23 +0800

    added additional cors origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: ['http://localhost:8000'],
+    origin: ['http://localhost:8000', 'http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     optionsSuccessStatus: 200
 };
